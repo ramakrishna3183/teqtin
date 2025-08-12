cb83d94a4e881e2fa605b0d72a61c04a26ecb476
Ramakrishna Dondapati <112241148+ramakrishna3183@users.noreply.github.com>
2025-08-12 11:17:01 +0530

    Add files via upload

---
+++ src/app/(user)/register/page.jsx
@@ -0,0 +1,14 @@
+
+
+import RegisterForm from '@/app/loginRegister/RegisterForm'
+import React from 'react'
+
+const UserRegister = () => {
+  return (
+    <div>
+         <RegisterForm />
+       </div>
+  )
+}
+
+export default UserRegister
